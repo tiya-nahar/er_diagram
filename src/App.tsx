c497c91759a98385c304508f9faffaa1c006db47
tiya-nahar <tiyanahar188@gmail.com>
2026-05-26 10:46:51 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="h-screen flex">
+      
+      <div className="w-[320px] bg-zinc-900 text-white p-4">
+        LEFT PANEL
+      </div>
+
+      <div className="flex-1 bg-zinc-800 text-white flex items-center justify-center">
+        DIAGRAM AREA
+      </div>
+
+    </div>
+  )
+}
+
+export default App
